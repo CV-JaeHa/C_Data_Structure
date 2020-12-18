4892fa7366cbc40c16a911a9a59bbc80bb97d8ce
taki0412 <taki041210@naver.com>
2020-12-18 16:28:42 +0900

    C Data Structure

---
+++ 리스트/이증 연결 리스트/이증 연결 리스트/이증 연결 리스트.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+typedef struct _node {
+	int data;
+	struct _node* prev;
+	struct _node* next;
+} Node;
+
+Node* head = NULL, * tail = NULL, * newNode = NULL;
+
+void InsertFront(Node* pNode, int data);
+void InsertBack(Node* pNode, int data);
+void DeleteFront(Node* pNode);
+void DeleteBack(Node* pNode);
+
+int main() {
+
+	return 0;
+}
+
+void InsertFront(Node* pNode, int data) {
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->data = data;
+	newNode->next = pNode->next;
+	pNode->next = newNode;
+	newNode->prev = pNode;
+}
+
+void InsertBack(Node* pNode, int data) {
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->data = data;
+	pNode->next = newNode;
+	newNode->prev = pNode;
+	newNode->next = tail;
+}
+
+void DeleteFront(Node* pNode) {
+	Node* temp;
+	temp = pNode->prev;
+	pNode->prev = pNode->prev->prev;
+	free(temp);
+}
+
+void DeleteBack(Node* pNode) {
+	Node* temp;
+	temp = pNode->next;
+	pNode->next = pNode->next->next;
+	free(temp);
+}
